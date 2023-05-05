599d3c7c4400a44f850b3414e3e688808682c165
RENNICK <michael.me@hotmail.co.uk>
2023-05-05 18:21:31 +0100

    Added force base class.
    GitOrigin-RevId: fe6bf0b002e21fb2877b9ef0acfec65efa0cb1df

---
+++ src/Forces/ForceBase.hh
@@ -0,0 +1,56 @@
+#pragma once
+#include<iostream>
+
+//ExternalForce.hh: Contains the force class for a constant applied body force in a given direction. This is
+//unfinished (should be able to specify magnitude and direction).
+
+template<typename placeholder=void>
+class ForceBaseTemplate{
+    public:
+
+        virtual double compute( const int xyz, const int k ) const; //Return force at lattice point k in direction xyz
+
+        virtual void precompute( const int k ); //Perform any neccessary computations before force is computed
+
+        virtual double computeDensitySource( const int k ) const; //Calculate any possible source/correction term for density
+
+        virtual double computeVelocitySource( const int xyz, const int k ) const; //Calculate any possible source/correction term for
+                                                           //velocity
+
+        virtual void postprocess( const int k ); //Perform any necessary postprocessing
+
+
+};
+
+template<typename placeholder>
+double ForceBaseTemplate<placeholder>::compute( const int xyz, const int k ) const{
+
+    return 0;
+
+}
+
+template<typename placeholder>
+void ForceBaseTemplate<placeholder>::precompute( const int k ){ //Not necessary
+    
+}
+
+template<typename placeholder>
+void ForceBaseTemplate<placeholder>::postprocess( const int k ){ //Not necessary
+    
+}
+
+template<typename placeholder>
+double ForceBaseTemplate<placeholder>::computeDensitySource( const int k ) const{ //Not necessary
+
+    return 0.0;
+
+}
+
+template<typename placeholder>
+double ForceBaseTemplate<placeholder>::computeVelocitySource( const int xyz, const int k) const{ //Need to correct velocity
+
+    return 0;
+    
+}
+
+typedef ForceBaseTemplate<> ForceBase;
