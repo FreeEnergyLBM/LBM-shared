21f084641a31138b773faa5e707837162f0bdcd2
RENNICK <michael.me@hotmail.co.uk>
2023-07-07 17:12:32 +0100

    Fixed flowfield_test.
    GitOrigin-RevId: af25da7bde50d98a39bb3f07802098bd3a4605c2

--- tests/unit/FlowField_test.cpp
+++ tests/unit/FlowField_test.cpp
@@ -7,9 +7,10 @@
 #include "Stencil.hh"
 
 TEST(FlowFieldTest, initialiseD2Q9) {
-  FlowField<LatticeProperties<Data1,NoParallel,1,1>> ff;
+  FlowField<LatticeProperties<Data1,NoParallel,2,2>> ff;
   ff.initialise();
 
   std::vector<double> equilibrium(D2Q9::Weights, D2Q9::Weights+9);
-  EXPECT_TRUE(ArraysMatch(ff.getDistribution(), equilibrium));
+  std::vector<double> distribution(&ff.getDistribution()[0], &ff.getDistribution()[0]+9);
+  EXPECT_TRUE(ArraysMatch(distribution, equilibrium));
 }
