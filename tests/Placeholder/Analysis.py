Fixed Dirichlet+Interpolated Dirichlet boundaries. Need to make some adjustments to gradients at the outer boundaries at some point.
GitOrigin-RevId: bb40b27a8b245c3bec5dbb29c71b14fd1ebedacf

tests/Placeholder/Analysis.py
@@ -101,17 +101,22 @@ for t in range(tstart,tend+1,tinc):
 
     im=ax.imshow(rgbv,interpolation='nearest',origin='upper')
     #im=ax.imshow(np.sqrt((gh.take(indices=0,axis=3).take(indices=slicepos,axis=sliceaxis))**2+(gh.take(indices=1,axis=3).take(indices=slicepos,axis=sliceaxis))**2),interpolation='nearest',origin='upper')
+    #im=ax.imshow(np.sqrt((gh.take(indices=0,axis=2).take(indices=slicepos,axis=sliceaxis))**2),interpolation='nearest',origin='upper')
     #im=ax.imshow(np.sqrt((v.take(indices=0,axis=3).take(indices=slicepos,axis=sliceaxis))**2+(v.take(indices=1,axis=3).take(indices=slicepos,axis=sliceaxis))**2),interpolation='nearest',origin='upper')
     #print(np.flip(rho.take(indices=slicepos,axis=sliceaxis)).transpose()[70,70])
     #ax.scatter(70,70)
     step=1
     X,Y=np.meshgrid(np.linspace(0,LX-1,int((LX)/step)),np.linspace(0,LY-1,int((LY)/step)))
-
+    #print(np.sum(np.sqrt((gh.take(indices=0,axis=3).take(indices=slicepos,axis=sliceaxis))**2+(gh.take(indices=1,axis=3).take(indices=slicepos,axis=sliceaxis))**2)))
     #ax.quiver(X.T,Y.T,np.flip(-v[:,:,:,0].take(indices=slicepos,axis=sliceaxis)),np.flip(v[:,:,:,3-sliceaxis].take(indices=slicepos,axis=sliceaxis)),width=0.001,headwidth=2.5,headlength=1.5)
+    print(np.sum(rho),1/6/(49)*np.log(1/(1-0.5)))
     fig.colorbar(im)
     #ax.scatter(49,49)
     plt.savefig(output, dpi=400, format='png')
     plt.close(fig)
+    plt.figure()
+    plt.plot(rho[:,int(LY/2),0])
+    plt.savefig("test_%012d.png"%(t), dpi=200, format='png')
 
 
 