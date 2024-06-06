95749f16c32a2322aec0e5e94e4f6cac67f86166
mehrdadvasheghanifarahani <mehrdadvasheghanifarahani@gmail.com>
2024-06-06 14:37:44 +0100

    Two-phase capillary-dominated flow example was added. The .cc file for the evaporation example in porous media was annotated.

--- examples/capillary_dominated_flow_2D/main.cc
+++ examples/capillary_dominated_flow_2D/main.cc
@@ -2,32 +2,33 @@
 
 #include <lbm.hh>
 
-// This script simulates evaporatin of a liquid (wetting phase) in a porous structure.
-// The non-wetting phase flows on a channel, contacting the wetting phase.
-// The evaporation rate is constant and adjusted by the parameter evaporationrate.
+// This script simulates two-phase capillary-dominated displacement through a porous structure in 2D.
+// This example shows how to:
+//      (i) pass the geometry to the lattice,
+//      (ii) use HaoCheng and Convective BCs for the phase field lattice, and
+//      (iii) define a region with no wettability to help with the stability of the displacement.
 
 const int lx = 250;  // Size of domain in x direction
 const int ly = 250;  // Size of domain in y direction
 
-const int timesteps = 5000000;   // Number of iterations to perform
-const int saveInterval = 50000;  // Interval to save global data
+const int timesteps = 1000000;   // Number of iterations to perform
+const int saveInterval = 20000;  // Interval to save global data
 
 // Parameters to control the surface tension and width of the diffuse interface
 // Surface tension (in lattice units) = sqrt(8*kappa*A/9)
 // Interface width (in lattice units) = sqrt(kappa/A)
-const double binaryA = 4.17e-3;
+// For reference, the model used is in section 9.2.2 of The Lattice Boltzmann Method: Principles and Practice, T. Kruger
+// et al. (2016)
+const double binaryA = 5.73e-3;
 const double binaryKappa = 2 * binaryA;
 const double contactAngle = 15;  // Contact angle of the liquid on the solid
 
 // Relaxation times of each component. tau1 corresponds to phi=1.0, tau2 corresponds to phi=-1.0
 // Viscosity (in lattice units) = 1.0/3.0 * (tau - 0.5)
-double tau1 = 0.621;
-double tau2 = 0.509;
+double tau1 = 0.533;
+double tau2 = 0.503;
 
 const double inletOrderParameter = -1.0;  // Order parameter value at the inlet
-const double inletVelocity = -2.0e-3;     // Velocity at the inlet
-
-const double evaporationrate = 1.0e-5;
 
 // Set up the lattice, including the resolution and data/parallelisation method
 using Lattice = LatticeProperties<ParallelX<1>, lx, ly>;
@@ -37,23 +38,22 @@ using Lattice = LatticeProperties<ParallelX<1>, lx, ly>;
 int initSolid(const int k) {
     int x = computeXGlobal<Lattice>(k);
     int y = computeY(ly, 1, k);
-
     if (y <= 1 || y >= ly - 2 || x <= 1 || x >= lx - 2)
         return 1;
-    else if (y <= 199)
-        return 0;
     else if (x == 2)
         return 3;
     else if (x == lx - 3)
         return 4;
-    return 0;
+    else
+        return 0;
 }
 
 // Function used to define the fluid
 // Here we set a tanh transition in the y direction. This should match the equilibrium profile.
 double initFluid(int k) {
-    int y = computeY(ly, 1, k);
-    if (y <= 1 || y >= ly - 2 || x <= 1 || x >= lx - 2) return -tanh((y - ly * .7) / (sqrt(2 * binaryKappa / binaryA)));
+    int x = computeXGlobal<Lattice>(k);
+    if (x == 2) return inletOrderParameter;
+    return tanh((x - lx / 10.) / (sqrt(2 * binaryKappa / binaryA)));
 }
 
 void unzipGeometry() {
@@ -70,14 +70,12 @@ int main(int argc, char **argv) {
     mpi.init();
 
     using TraitFlowFieldBinary = DefaultTraitFlowFieldBinary<Lattice>::SetCollisionOperator<MRT>::SetBoundary<
-        std::tuple<VelocityInflow, Convective>, std::tuple<BounceBack>>;
-
+        std::tuple<BounceBack>, std::tuple<VelocityInflow, Convective>>;
     using TraitBinary = DefaultTraitBinary<Lattice>::template SetProcessor<
         std::tuple<ConvectOP>, std::tuple<GradientsMultiStencil<OrderParameter<>, CentralXYZ, LaplacianCentral>>,
-        std::tuple<ChemicalPotentialCalculatorBinary, CubicWetting, SimpleMassLossCalculator,
+        std::tuple<ChemicalPotentialCalculatorBinary, CubicWetting,
                    SetParameterOld<OrderParameter<>, OrderParameterOld<>>>>::
-        AddForce<EvaporationPhaseSource<EvaporationSourceMethod>>::SetBoundary<
-            std::tuple<DirichletOrderParameter, Convective>, std::tuple<BounceBack>>;
+        SetBoundary<std::tuple<BounceBack>, std::tuple<HaoCheng, Convective>>;
 
     // Define the models to be used
     FlowFieldBinary<Lattice, TraitFlowFieldBinary>
@@ -98,8 +96,6 @@ int main(int argc, char **argv) {
     componentSeparationModel.getProcessor<ChemicalPotentialCalculatorBinary>().setA(binaryA);
     componentSeparationModel.getProcessor<ChemicalPotentialCalculatorBinary>().setKappa(binaryKappa);
 
-    componentSeparationModel.getProcessor<SimpleMassLossCalculator>().setEvaporationRate(evaporationrate);
-
     // Define the solid boundaries
     unzipGeometry();
     Geometry<Lattice>::importLabels("geometry.dat");
@@ -108,7 +104,7 @@ int main(int argc, char **argv) {
     componentSeparationModel.getBoundary<BounceBack>().setNodeID(1);
 
     std::vector<double> u(3);
-    u[0] = inletVelocity;
+    u[0] = -0.0001;
     u[1] = 0.;
     u[2] = 0.;
     flowFieldModel.getBoundary<VelocityInflow>().setNodeID(3);
@@ -116,14 +112,11 @@ int main(int argc, char **argv) {
 
     flowFieldModel.getBoundary<Convective>().setNodeID(4);
 
-    componentSeparationModel.getBoundary<DirichletOrderParameter>().setNodeID(3);
-    componentSeparationModel.getBoundary<DirichletOrderParameter>().setInterfaceVal(inletOrderParameter);
+    componentSeparationModel.getBoundary<HaoCheng>().setNodeID(3);
+    componentSeparationModel.getBoundary<HaoCheng>().setPhi(inletOrderParameter);
 
     componentSeparationModel.getBoundary<Convective>().setNodeID(4);
     componentSeparationModel.getProcessor<ConvectOP>().setNodeID(4);
-
-    flowFieldModel.setCollideID({0});
-    componentSeparationModel.setCollideID({0});
 
     componentSeparationModel.getProcessor<CubicWetting>().setNodeID(1);
     componentSeparationModel.getProcessor<CubicWetting>().setThetaDegrees(contactAngle);
@@ -146,16 +139,16 @@ int main(int argc, char **argv) {
     for (int timestep = 0; timestep <= timesteps; timestep++) {
         // Save the desired parameters, producing a binary file for each.
         if (timestep % saveInterval == 0) {
-            std::cout << "Saving at timestep " << timestep << "." << std::endl;
             saver.saveVTK(timestep, Density<>::template getInstance<Lattice>(),
                           OrderParameter<>::template getInstance<Lattice>(),
                           Velocity<>::template getInstance<Lattice, Lattice::NDIM>());
+
+            std::cout << "Saving at timestep " << timestep << "." << std::endl;
         }
 
         // Evolve by one timestep
         lbm.evolve();
     }
 
-    std::cout << "Simulation complete." << std::endl;
     return 0;
 }
