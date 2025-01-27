5ff446422641b6d7315231f150eb35a083fb40c0
mehrdadvasheghanifarahani <mehrdadvasheghanifarahani@gmail.com>
2025-01-27 19:42:07 +0000

    BB class was modified: BB for HLBM was removed instead a BB class with NodeID for fluids was added.

--- src/BoundaryModels/BounceBack.hh
+++ src/BoundaryModels/BounceBack.hh
@@ -23,24 +23,28 @@ inline void BounceBack::compute(TDistributionType& distribution, int k) {
     }
 }
 
-// Not different from BounceBack at the moment, but it is used in HLBM model and might
-// become different in the future
-class BounceBackHLBM : public BoundaryBase {
+// This class has the same functionality as BounceBack class, but applies the bounce-back BC by setting NodeID to fluid nodes.
+class BounceBackWithFluidNodeID : public BoundaryBase {
    public:
     template <class TTraits, class TDistributionType>
     inline void compute(TDistributionType& mDistribution, int k);
+    inline void setBoundaryID(int id) { boundaryID = id; }
+
+   private:
+    int boundaryID = 1;
 };
 
 template <class TTraits, class TDistributionType>
-inline void BounceBackHLBM::compute(TDistributionType& distribution, int k) {
+inline void BounceBackWithFluidNodeID::compute(TDistributionType& distribution, int k) {
     using Lattice = typename TTraits::Lattice;
 
     if (!this->apply<Lattice>(k)) return;
 
     for (int idx = 1; idx < TTraits::Stencil::Q; idx++) {
-        if (this->apply<Lattice>(distribution.streamIndex(k, idx))) continue;
-
-        distribution.getDistributionPointer(distribution.streamIndex(k, idx))[idx] =
-            distribution.getPostCollisionDistribution(distribution.streamIndex(k, idx), distribution.getOpposite(idx));
+        if (Geometry<Lattice>::getBoundaryType(distribution.streamIndex(k, distribution.getOpposite(idx))) ==
+            boundaryID) {
+            distribution.getDistributionPointer(k)[idx] =
+                distribution.getPostCollisionDistribution(k, distribution.getOpposite(idx));
+        }
     }
 }
